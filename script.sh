f6a14d2c32b55be6aa12c56678782911bfd83fc9
root <root@mokus-VirtualBox>
2022-11-18 11:59:52 +0300

    upload

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 while read line; do 
-git clone ssh://git@stash.mvideo.ru:7999/rtd/$line.git >/dev/null 2>&1
+git clone ssh://git@stash.****.ru:7999/rtd/$line.git >/dev/null 2>&1
 if [ $? -eq 128 ]
 then
 echo "=================================================================" $'\n'$line $'\n'"Do Fetch."
 cd $line
-git fetch ssh://git@stash.mvideo.ru:7999/rtd/$line.git >/dev/null 2>&1
+git fetch ssh://git@stash.****.ru:7999/rtd/$line.git >/dev/null 2>&1
 git log origin/master..develop | grep -o RTD-'[0-9][0-9][0-9][0-9][0-9]' | awk '{ for (i=1;i<=NF;i++ ) printf $i ", " }' | rev | cut -c 3- | rev
 git describe origin/develop --tags
 cd ..
